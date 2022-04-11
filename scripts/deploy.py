6d7f46880ef7dce2a9c0a1a72547abf6c4f02f51
ShichidaNael <nael.shichida@gmail.com>
2022-04-11 21:45:04 +0100

    first commit

---
+++ scripts/deploy.py
@@ -0,0 +1,34 @@
+from unittest.mock import Mock
+from brownie import FundMe, network, config, MockV3Aggregator
+from scripts.helpful_scripts import (
+    deploy_mocks,
+    get_account,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+)
+from web3 import Web3
+
+
+def deploy_fund_me():
+    account = get_account()
+    # pass the price feed address to our fundme contract
+
+    # if we are on a persistent network like rinkeby, use the associated address
+    # otherwise, deploy mocks
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else:
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
+
+    fund_me = FundMe.deploy(
+        "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
+    print(f"Contact deployed to {fund_me.address}")
+
+
+def main():
+    deploy_fund_me()
